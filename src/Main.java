9d5df712b6d67feacdadbab2a2622cdb7a2793fa
Montse <montsemsanz@gmail.com>
2020-02-23 12:22:40 +0100

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Proyecto partida prueba Git Eclipse");
 		System.out.println("Probando Git desde eclipse");
+		System.out.println("segundo cambio");
 
 	}
 
