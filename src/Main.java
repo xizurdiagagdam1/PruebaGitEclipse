2f1716e861128e8afdf0f2b23609908e9607179b
Montse <montsemsanz@gmail.com>
2020-02-23 12:21:11 +0100

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Proyecto partida prueba Git Eclipse");
+		System.out.println("Probando Git desde eclipse");
 
 	}
 
